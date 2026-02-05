a0547525787aeb515f674efaf47566f96636143d
AmyCV15 <chavezamairani@gmail.com>
2026-02-05 17:33:28 -0600

    Primera parte de la tarea

---
+++ Tarea1_Bioinfo_posgrado/Tarea1_bioinfo_posgrado_cap1.R
@@ -0,0 +1,10 @@
+####### Tarea 1: Bioinfo posgrado#### 
+#### Ejercicios del capítulo 1 ###
+# 1. "Instala las siguientes librerías que te serviran durante todo el curso"
+install.packages("markdown")
+library(markdown)
+install.packages("ggplot2")
+library(ggplot2)
+#de esto no me acordaba: lo guardaré aquí para recordarlo
+#para saber directorio: getwd()
+#para cambiar de directorio: setwd("direccion_a_la_que_quieres_ir")
