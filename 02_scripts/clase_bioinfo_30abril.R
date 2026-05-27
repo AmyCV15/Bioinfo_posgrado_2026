e54a8f52b738c36e8a67ae003882e58c5b137079
AmyCV15 <chavezamairani@gmail.com>
2026-05-27 11:43:13 -0600

    Clase 30 de abril.

---
+++ 02_scripts/clase_bioinfo_30abril.R
@@ -0,0 +1,8 @@
+### clase del 30426 ##
+#arcchivo fast q identificador @ seq_ID
+#segunda liena secuencia 
+#cuarta secuencia identificadores de claidad para cada base -- si esto se pasa a codigo numerico se puede 
+#obtener un valor que se llama calidad fred. 
+#cada signo tiene un numero en el codigo ascii y entonces de ahí se obtiene el valor
+#QPHRED = -10xlog10(pe)
+#biojupies
